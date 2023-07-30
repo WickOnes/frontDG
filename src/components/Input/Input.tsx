f8817edf95ee66bdf17ef65a150672f6fad0a7f8
olkovmark <olkovmark@gmail.com>
2023-07-30 14:38:48 +0200

    update input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,14 @@
 import "./Input.scss";
 
-export const Input = ({ title, className, ...props }: any) => {
+export const Input = ({ title, className, onChange, ...props }: any) => {
   return (
     <div className={`input_block ${className}`}>
       {title ? <h3 className="title">{title}</h3> : null}
-      <input className="input" {...props} />
+      <input
+        className="input"
+        onChange={(v) => onChange([v.target.name, v.target.value])}
+        {...props}
+      />
     </div>
   );
 };
